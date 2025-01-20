1958b6a10d6480daa1261e16187feec84fc4935d
Brayan-chan <al071392@uacam.mx>
2025-01-19 18:22:13 -0600

    Agregar soporte para módulos ES y CommonJS en socket.io y engine.io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 import express from "express";
 import logger from "morgan";
+import { Server } from 'socket.io';
+import  { createServer } from 'node:http';
 
 const port = process.env.PORT ?? 3000;
 
 const app = express();
 app.use(logger('dev'));
+
+// Se crea un servidor con el paquete http de Node.js
+const server = createServer(app);
+
+// Se crea una instancia de Socket.io y se le pasa el servidor creado
+const io = new Server(server);
+
+// Se define el evento 'connection' que se ejecuta cuando un cliente se conecta al servidor
+io.on('connection', () => {
+    console.log('a user has connected');
+});
 
 app.get('/', (req, res) => {
     // Ejemplo para servir una respuesta HTML
@@ -15,6 +28,10 @@ app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server started on port ${port}`);
-});
+})
+
+/*app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});*/
